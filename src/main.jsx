94fc4f3e0e7da294a4403754b9b8e869092a766e
joy_lee <purple402103@gmail.com>
2025-05-15 17:44:48 +0900

    feat : 기본 프로젝트 세팅

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import './assets/scss/common.scss'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
 )
