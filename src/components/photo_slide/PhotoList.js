309bddc419619fb8e956810a181560c613c3e1e2
joy_lee <purple402103@gmail.com>
2025-06-01 03:38:36 +0900

    feat : 이미지 변경, 순서 정렬 등

--- src/components/photo_slide/PhotoList.js
+++ src/components/photo_slide/PhotoList.js
@@ -4,12 +4,20 @@ const imageArray = Object.entries(images).map(([path, mod]) => {
   const parts = path.split('/');
   const fileName = parts[parts.length - 1];
   const name = fileName.slice(0, 10).replaceAll('-', '.');
+  const order = fileName.match(/\((\d+)\)/); // () 으로 순서가 정해져있는지 확인F
 
   return {
     date: name,
     src: mod.default, // 실제 이미지 경로
+    order: order ? parseInt(order[1], 10) : 1,
   };
 });
-imageArray.sort((a, b) => new Date(b.date) - new Date(a.date));
+imageArray.sort((a, b) => {
+  if (b.date !== a.date) {
+    return new Date(b.date) - new Date(a.date);
+  } else {
+    return a.order - b.order;
+  }
+});
 
 export default imageArray;
