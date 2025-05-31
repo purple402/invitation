ae19bf3d9eba42cc62388a17626a5da80ed09578
joy_lee <purple402103@gmail.com>
2025-06-01 00:47:17 +0900

    fix : env.production 의 VITE_BASE_APP_URL 변경

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const BASE_APP_URL = import.meta.env.VITE_BASE_APP_URL;
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: '/invitation',
     element: <MainPage />,
   },
   { basename: BASE_APP_URL },
