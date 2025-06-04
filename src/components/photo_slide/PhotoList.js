3c4831dbaaa71b7b6450ada1ea254a8893e328e1
joy_lee <purple402103@gmail.com>
2025-06-04 14:35:04 +0900

    feat : 이미지 목록 순서 변경

--- src/components/photo_slide/PhotoList.js
+++ src/components/photo_slide/PhotoList.js
@@ -3,21 +3,16 @@ const images = import.meta.glob('../../assets/images/photo/*.{jpg,jpeg}', { eage
 const imageArray = Object.entries(images).map(([path, mod]) => {
   const parts = path.split('/');
   const fileName = parts[parts.length - 1];
-  const name = fileName.slice(0, 10).replaceAll('-', '.');
-  const order = fileName.match(/\((\d+)\)/); // () 으로 순서가 정해져있는지 확인F
+  const [order, dateInfo] = fileName.split('_'); // ['order', 'date.jpeg']
+  const [date, _] = dateInfo.split('.'); // ['date', 'jpeg']
 
   return {
-    date: name,
     src: mod.default, // 실제 이미지 경로
-    order: order ? parseInt(order[1], 10) : 1,
+    order: parseInt(order),
+    date: date.replaceAll('-', '.'),
   };
 });
-imageArray.sort((a, b) => {
-  if (b.date !== a.date) {
-    return new Date(b.date) - new Date(a.date);
-  } else {
-    return a.order - b.order;
-  }
-});
+
+imageArray.sort((a, b) => a.order - b.order);
 
 export default imageArray;
